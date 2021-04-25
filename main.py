139310724cc60c90e66b302f17f018f5b04c5c22
Xanops <pzvpetr@gmail.com>
2021-04-25 19:37:32 +0300

    New design of the main page

--- main.py
+++ main.py
@@ -8,39 +8,14 @@ def index():
     return render_template('podborka.html', title='Всё что нужно - любые товары самых разных категорий.')
 
 
-@app.route('/personal_cabinet')
-def personal_cabinet():
-    return render_template('personal_cabinet.html', title='Личный кабинет')
-
-
 @app.route('/shopping_cart')
 def basket():
     return render_template('shopping_cart.html', title='Корзина')
 
 
-@app.route('/delivery')
-def delivery():
-    return render_template('delivery.html', title='Доставка')
-
-
-@app.route('/pick-up_service')
-def pick_up_service():
-    return render_template('pick-up_service.html', title='Самовывоз')
-
-
-@app.route('/contacts')
-def contacts():
-    return render_template('contacts.html', title='Контакты')
-
-
-@app.route('/phones')
-def smartphones():
-    return render_template('phones.html', title='Смартфоны')
-
-
-@app.route('/laptops')
-def laptops():
-    return render_template('laptops.html', title='Ноутбуки')
+@app.route('/category/<string:category>')
+def product_category(category):
+    return render_template('phones.html', title='')
 
 
 @app.route('/register_user')
